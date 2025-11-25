23f21c0233b6b385d569a2b34dfeeb0740c516e3
srinivaskinik <32127018+srinivaskinik@users.noreply.github.com>
2025-11-25 17:35:50 +0530

    Uncomment style tag for visual regression test

--- tests/visual-change.spec.ts
+++ tests/visual-change.spec.ts
@@ -4,12 +4,12 @@ test('local visual regression check', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/#/');
 
   // Simulated UI change
-  // await page.addStyleTag({
-  //   content: `
-  //     h1 { color: blue !important; }
-  //     .new-todo { border: 2px solid blue !important; }
-  //   `
-  // });
+   await page.addStyleTag({
+     content: `
+       h1 { color: blue !important; }
+       .new-todo { border: 2px solid blue !important; }
+     `
+   });
 
   // This will fail locally if UI differs
   await expect(page).toHaveScreenshot('homepage.png');
