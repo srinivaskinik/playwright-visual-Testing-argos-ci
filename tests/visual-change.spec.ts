015af2d55537a3978d6c5963033fada5f695ff84
srinivaskinik <32127018+srinivaskinik@users.noreply.github.com>
2025-11-25 17:42:53 +0530

    Update visual-change.spec.ts

--- tests/visual-change.spec.ts
+++ tests/visual-change.spec.ts
@@ -3,13 +3,7 @@ import { test, expect } from '@playwright/test';
 test('local visual regression check', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/#/');
 
-  // Simulated UI change
-  // await page.addStyleTag({
-  //   content: `
-   //    h1 { color: blue !important; }
-    //   .new-todo { border: 2px solid blue !important; }
-     `
-  // });
+
 
   // This will fail locally if UI differs
   await expect(page).toHaveScreenshot('homepage.png');
