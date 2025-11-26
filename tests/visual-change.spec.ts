cd30ee5c504aa89f0fc77e026a0f6d57a0d3e1bc
srinivaskinik <32127018+srinivaskinik@users.noreply.github.com>
2025-11-26 10:37:38 +0530

    Update visual-change.spec.ts

--- tests/visual-change.spec.ts
+++ tests/visual-change.spec.ts
@@ -4,7 +4,13 @@ import { argosScreenshot } from '@argos-ci/playwright';
 test('@visual @snapshot local visual regression check', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/#/');
 
-
+  // Simulated UI change
+  await page.addStyleTag({
+    content: `
+      h1 { color: blue !important; }
+      .new-todo { border: 2px solid blue !important; }
+    `
+  });
 
   // This will fail locally if UI differs
 //  await expect(page).toHaveScreenshot('homepage.png');
